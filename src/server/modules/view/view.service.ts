9a54debe8752a2637e51f1ff3bf3de94ee0c5d8b
tuyennx <xuantuyen.nguyen@vmodev.com>
2023-01-18 15:24:44 +0700

    fix lint

--- src/server/modules/view/view.service.ts
+++ src/server/modules/view/view.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import {Injectable, OnModuleInit} from '@nestjs/common';
 import next from 'next';
 
 @Injectable()
@@ -7,7 +7,7 @@ export class ViewService implements OnModuleInit {
 
   async onModuleInit(): Promise<void> {
     try {
-      this.server = next({ dev: true, dir: './src/client' });
+      this.server = next({dev: true, dir: './src/client'});
       await this.server.prepare();
     } catch (error) {
       console.log(error);
